35b2467cce20a0578d901c3410e0a8b2475055cc
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-09-23 21:55:05 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# ----------------- CONFIGURATION ----------------- #
+
+# Get these values from my.telegram.org
+API_ID = 12345678
+API_HASH = ""
+
+# Get this from @BotFather on Telegram
+BOT_TOKEN = ""
+
+# The channel username (without @) that users must join to use the bot.
+# The bot MUST be an admin in this channel to check membership.
+MUST_JOIN = "TNCnetwork"
+
+# ------------------------------------------------- #
