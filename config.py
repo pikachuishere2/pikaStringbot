d00230915ab6d0083868cb6a0d4e0a0e42973492
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-09-23 22:47:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # ----------------- CONFIGURATION ----------------- #
 
 # Get these values from my.telegram.org
-API_ID = 12345678
-API_HASH = ""
+API_ID = 22657083
+API_HASH = "d6186691704bd901bdab275ceaab88f3"
 
 # Get this from @BotFather on Telegram
-BOT_TOKEN = ""
+BOT_TOKEN = "8337321628:AAElnUWD4YCgRF9EBKWSWtmBuWWTZfJNQy0"
 
 # The channel username (without @) that users must join to use the bot.
 # The bot MUST be an admin in this channel to check membership.
