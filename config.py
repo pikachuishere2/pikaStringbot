03b4b876daf995cfb1f94d657ce362c3425bd978
pikachuishere2 <escrowertaushif@gmail.com>
2025-10-28 10:35:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # ----------------- CONFIGURATION ----------------- #
 
 # Get these values from my.telegram.org
-API_ID = 22657083
-API_HASH = "d6186691704bd901bdab275ceaab88f3"
+API_ID = ""
+API_HASH = ""
 
 # Get this from @BotFather on Telegram
-BOT_TOKEN = "8337321628:AAElnUWD4YCgRF9EBKWSWtmBuWWTZfJNQy0"
+BOT_TOKEN = ""
 
 # The channel username (without @) that users must join to use the bot.
 # The bot MUST be an admin in this channel to check membership.
-MUST_JOIN = "TechNodeCoders"
+MUST_JOIN = "PikaBotUodates"
 
 # ------------------------------------------------- #
